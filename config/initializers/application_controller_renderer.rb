859f67a4379b01c82b7cb3c4f5cc00e5c7d944db
misakisu <che.v.jq@gmail.com>
2020-04-21 00:14:08 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b5338dc6024074d8bfe51a29593a4e0f5887cb68718eee459061bbde55e01f5ecad4c3047b7ef2f107a2b51f2f736916b31f2344846ed0bcabf8e523017234fe'
